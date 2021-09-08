689db002933e03633edfa80a23fd0849a32f1312
Satho <mehsatho@gmail.com>
2021-09-08 18:27:46 +0100

    Used for character selection on login.

---
+++ Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingC
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public string Portrait { get; set; }
+        public int ID { get; set; }
+    }
+}
